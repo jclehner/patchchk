7e1a39a30cd31d3b43e5f51d7138593dc00a7792
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2022-01-05 23:22:31 +0100

    Add MSVC build script

---
+++ build-release-msvc.bat
@@ -0,0 +1,7 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+rmdir /S /Q build
+mkdir build
+cd build
+cmake -G "NMake Makefiles" ..
+nmake package
