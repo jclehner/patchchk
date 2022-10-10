c579fb49d2b66ffa923d13577beeda28b32f7cd4
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2022-10-10 17:10:41 +0200

    Add build script for mingw on Linux

---
+++ build-release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CMAKE_ARGS=
+BUILD_DIR=build
+
+if [ "$1" = "mingw" ]; then
+	CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=i686-w64-mingw32.cmake"
+	BUILD_DIR=build-win32
+fi
+
+rm -rf "$BUILD_DIR"
+mkdir "$BUILD_DIR"
+cd "$BUILD_DIR"
+cmake $CMAKE_ARGS ..
+make package
