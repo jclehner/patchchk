b2b1edc71a5d3cf4fe05dc03697803d7dc7027e4
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2023-01-11 17:03:04 +0100

    Use "Release" build type on Windows
    In response to #1.

--- build-release-msvc.bat
+++ build-release-msvc.bat
@@ -3,6 +3,6 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDe
 rmdir /S /Q build
 mkdir build
 cd build
-cmake -G "NMake Makefiles" ..
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 nmake package
 cd ..
